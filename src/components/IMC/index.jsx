33c0570d848da7079503e37f47427b2238a6156a
Paulo Cesar <paulo_vieira22@hotmail.com>
2025-07-14 13:32:23 -0300

    evolucao

--- src/components/IMC/index.jsx
+++ src/components/IMC/index.jsx
@@ -1,18 +1,53 @@
 import './index.css'
+import { useState } from 'react';
 
 const IMC = () => {
+
+    const [height, setHeight] = useState('')
+    const [weight, setWeight] = useState('')
+    const [release, setRealise] = useState(false)
+    const [releaseWeight, setReleaseWeight] = useState(false)
+    const [releaseHeight, setReleaseHeight] = useState(false)
+
+    const handleHeight = (event) => {
+        setHeight(event.target.value);
+        setReleaseHeight(true)
+    };  
+    
+    const handleWeight = (event) => {
+        setWeight(event.target.value);
+        setReleaseWeight(true)
+    };
+
+
+    const handleclick = (e) => {
+        e.preventDefault();
+        setRealise(true)
+
+    }
+
+    const final = () => {
+        //aplicar const imc = peso / (altura * altura);
+
+        return Number(height) + Number(weight)
+    }
     return(
         <>
             <h1>IMC</h1>
             <form className="form">
                 <label>Qual sua Altura?</label>
-                <input type="float" />
+                <input type="number" value={height} onChange={handleHeight}/>
 
                 <label htmlFor="">Qual seu peso</label>
-                <input type="number" />
+                <input type="number" value={weight} onChange={handleWeight}/>
 
-                <button>IMC</button>
+                <button onClick={handleclick}>IMC</button>
+
+                {release && (
+                    <p>Soma: {final()}</p>
+                )}
             </form>
+
         </>
     )
 }
